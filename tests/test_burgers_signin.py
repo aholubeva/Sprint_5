73112f4c1d57dbe9333dfa06f22176829bb81056
Anastasiya Holubeva <nastyaa.holubeva@gmail.com>
2024-09-02 11:26:02 +0300

    email and password for login were saved in helpers.py in additional function

--- tests/test_burgers_signin.py
+++ tests/test_burgers_signin.py
@@ -2,14 +2,16 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from src.config import Config
 from src.locators import BurgersLocators
+from src.helpers import get_sign_in_data
 
 class TestBurgersSignIn:
 
     def test_sign_in_from_main_screen(self, driver):
         driver.get(Config.URL)
+        email_data, password_data = get_sign_in_data()
         driver.find_element(*BurgersLocators.SIGN_IN).click()
-        driver.find_element(*BurgersLocators.EMAIL_FIELD).send_keys("anastasiyagolubeva13123@yandex.ru")
-        driver.find_element(*BurgersLocators.PASSWORD_FIELD).send_keys("123456")
+        driver.find_element(*BurgersLocators.EMAIL_FIELD).send_keys(email_data)
+        driver.find_element(*BurgersLocators.PASSWORD_FIELD).send_keys(password_data)
         driver.find_element(*BurgersLocators.SUBMIT_BUTTON).click()
         order_button = WebDriverWait(driver, Config.TIMEOUT).until(expected_conditions.visibility_of_element_located(BurgersLocators.ORDER_BUTTON))
         assert order_button.is_displayed()
@@ -17,9 +19,10 @@ class TestBurgersSignIn:
 
     def test_sign_in_from_profile_button(self, driver):
         driver.get(Config.URL)
+        email_data, password_data = get_sign_in_data()
         driver.find_element(*BurgersLocators.PROFILE_BUTTON).click()
-        driver.find_element(*BurgersLocators.EMAIL_FIELD).send_keys("anastasiyagolubeva13123@yandex.ru")
-        driver.find_element(*BurgersLocators.PASSWORD_FIELD).send_keys("123456")
+        driver.find_element(*BurgersLocators.EMAIL_FIELD).send_keys(email_data)
+        driver.find_element(*BurgersLocators.PASSWORD_FIELD).send_keys(password_data)
         driver.find_element(*BurgersLocators.SUBMIT_BUTTON).click()
         order_button = WebDriverWait(driver, Config.TIMEOUT).until(expected_conditions.visibility_of_element_located(BurgersLocators.ORDER_BUTTON))
         assert order_button.is_displayed()
@@ -27,9 +30,10 @@ class TestBurgersSignIn:
 
     def test_sign_in_from_registration_button(self, driver):
         driver.get(f'{Config.URL}/register')
+        email_data, password_data = get_sign_in_data()
         driver.find_element(*BurgersLocators.SIGN_IN_ALREADY).click()
-        driver.find_element(*BurgersLocators.EMAIL_FIELD).send_keys("anastasiyagolubeva13123@yandex.ru")
-        driver.find_element(*BurgersLocators.PASSWORD_FIELD).send_keys("123456")
+        driver.find_element(*BurgersLocators.EMAIL_FIELD).send_keys(email_data)
+        driver.find_element(*BurgersLocators.PASSWORD_FIELD).send_keys(password_data)
         driver.find_element(*BurgersLocators.SUBMIT_BUTTON).click()
         order_button = WebDriverWait(driver, Config.TIMEOUT).until(expected_conditions.visibility_of_element_located(BurgersLocators.ORDER_BUTTON))
         assert order_button.is_displayed()
@@ -37,9 +41,10 @@ class TestBurgersSignIn:
 
     def test_sign_in_from_forgot_password(self, driver):
         driver.get(f'{Config.URL}/forgot-password')
+        email_data, password_data = get_sign_in_data()
         driver.find_element(*BurgersLocators.SIGN_IN_ALREADY).click()
-        driver.find_element(*BurgersLocators.EMAIL_FIELD).send_keys("anastasiyagolubeva13123@yandex.ru")
-        driver.find_element(*BurgersLocators.PASSWORD_FIELD).send_keys("123456")
+        driver.find_element(*BurgersLocators.EMAIL_FIELD).send_keys(email_data)
+        driver.find_element(*BurgersLocators.PASSWORD_FIELD).send_keys(password_data)
         driver.find_element(*BurgersLocators.SUBMIT_BUTTON).click()
         order_button = WebDriverWait(driver, Config.TIMEOUT).until(expected_conditions.visibility_of_element_located(BurgersLocators.ORDER_BUTTON))
         assert order_button.is_displayed()
